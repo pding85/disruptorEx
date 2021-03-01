8df4be06e0d585e15edf695cefb819356f265802
pding85@163.com <dingpeng1234>
2021-03-01 22:04:30 +0800

    disruptor 知識點梳理

---
+++ disruptor/src/test/java/com/pding85/allocation/ListTest.java
@@ -0,0 +1,121 @@
+package com.pding85.allocation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+public class ListTest {
+
+    static Map<String, List<String>> container = new ConcurrentHashMap<>();
+    static String KEY = "KEY-001" ;
+
+
+    public static void iteration1(List<String> data) {
+
+        if (data!=null && data.size() > 0) {
+            container.put(KEY, data);
+        }
+
+        int size = 0;
+        try {
+            int len = 1;
+            for (String ddd : data) {
+                if (++len % 600 == 0) {
+                    sleep2(1);
+                }
+                // sleep2(1);
+                ++size;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(size);
+    }
+
+
+    public static void iteration2() {
+        //
+        List<String> valueList = container.get(KEY);
+       //
+        for (int i = 0 ; i < 100; ++i) {
+            valueList.add("@222222");
+        }
+        sleep1(1);
+        valueList.add("@1222222");
+        sleep1(1);
+        valueList.add("@1222222");
+        valueList.add("@1222222");
+        valueList.add("@222222");
+        valueList.add("@1222222");
+        valueList.add("@1222222");
+
+        iteration1(valueList);
+    }
+
+    public static void sleep1(int mill) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(mill);
+        } catch (Exception e) {}
+    }
+
+    public static void sleep2(int mill) {
+        try {
+            TimeUnit.NANOSECONDS.sleep(mill * 1);
+        } catch (Exception e) {}
+    }
+
+    public static List<String> newData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0 ; i < 1000; ++i) {
+            data.add(i + "");
+        }
+
+        return data;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+
+                while (true) {
+
+                    try {
+
+                        ListTest.iteration1(newData());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        sleep1(1);
+                    }
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                sleep1(10);
+                while (true) {
+
+                    try {
+
+                        iteration2();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        sleep1(1);
+                    }
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+
+    }
+}
---
+++ disruptor/src/test/java/com/pding85/allocation/TaskThread.java
@@ -0,0 +1,148 @@
+package com.pding85.allocation;
+
+import org.apache.commons.lang.time.StopWatch;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TaskThread extends Thread  {
+
+    Lock lock = new ReentrantLock();
+    Condition signal = lock.newCondition();
+    Queue<Long> queue = new ConcurrentLinkedQueue<>();
+    static Unsafe unsafe;
+    static final Long count = 1000_000L;
+    AtomicBoolean status = new AtomicBoolean(false);
+
+    static {
+
+        try {
+            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            unsafe = (Unsafe) f.get(null);
+        } catch (Exception e) {}
+    }
+
+
+    public void park() {
+        try {
+            lock.lock();
+            signal.wait();
+        } catch (Exception e) {
+
+        } finally {
+            try{
+                lock.unlock();
+            } catch (Exception e) {}
+        }
+    }
+
+    public void park2() {
+
+        unsafe.park(false, 0);
+    }
+
+    public void offer2(Long sequence) {
+        try {
+            queue.offer(sequence);
+
+                // unsafe.unpark(this);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void offer(Long sequence) {
+        try {
+            lock.lock();
+            queue.offer(sequence);
+            signal.signalAll(); // 不是每次必须的，浪费性能
+        } catch (Exception e) {
+
+        } finally {
+            try{
+                lock.unlock();
+            } catch (Exception e) {}
+        }
+    }
+
+    @Override
+    public void run() {
+        long i = 0L ;
+        while (true) {
+
+            try {
+                if (queue.isEmpty()) {
+                    park();
+                }
+
+                Long data = queue.poll();
+
+                if (data % 10000 ==0)
+                    System.out.println(data);
+
+                if (count <= data) {
+                    return;
+                }
+
+
+            } catch (Exception e) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (Exception exception) {}
+            }
+
+        }
+    }
+
+    // 测试代码
+    public static void main(String[] args) {
+        //
+        TaskThread center = new TaskThread();
+
+        Thread producer = new Thread() {
+            volatile  Long i = 0L;
+
+            @Override
+            public void run() {
+
+                while (true) {
+
+                    center.offer(++i);
+                    if (i >= count) {
+                        return;
+                    }
+                }
+            }
+        };
+        producer.start();
+        center.start();
+
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        try {
+            producer.join();
+        } catch (Exception e) {}
+
+        try {
+           center.join();
+        } catch (Exception e) {}
+
+        stopWatch.stop();
+        System.out.println(stopWatch.getTime());
+
+    }
+}
